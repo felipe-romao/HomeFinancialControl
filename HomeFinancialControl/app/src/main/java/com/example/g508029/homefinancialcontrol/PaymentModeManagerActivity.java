def9857b7eaccbf15f456bdfe2b781a694f06968
felipe-romao <felipe.romao@gmail.com>
2019-02-04 23:23:09 -0200

    Create payment mode manager activity

---
+++ HomeFinancialControl/app/src/main/java/com/example/g508029/homefinancialcontrol/PaymentModeManagerActivity.java
@@ -0,0 +1,13 @@
+package com.example.g508029.homefinancialcontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PaymentModeManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_mode_manager);
+    }
+}
