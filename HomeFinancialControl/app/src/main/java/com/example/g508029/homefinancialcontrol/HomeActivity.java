d3593bb7701c453fe86ab622c41c311105265874
felipe.romao <felipe.romao@gmail.com>
2019-12-31 00:30:38 -0300

    Add new features

--- HomeFinancialControl/app/src/main/java/com/example/g508029/homefinancialcontrol/HomeActivity.java
+++ HomeFinancialControl/app/src/main/java/com/example/g508029/homefinancialcontrol/HomeActivity.java
@@ -1,11 +1,29 @@
 package com.example.g508029.homefinancialcontrol;
 
 import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActionBar supportActionBar = getSupportActionBar();
+        supportActionBar.setDisplayHomeAsUpEnabled(true);
+        supportActionBar.setHomeAsUpIndicator(R.mipmap.finance);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -17,6 +35,18 @@ public class HomeActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         switch (id){
+            case android.R.id.home:
+                Intent mainIntent = new Intent(this, MainActivity.class);
+                mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
+                    finishAffinity();
+                }
+
+                startActivity(mainIntent);
+                break;
+
             case R.id.main_menu_payment_mode_manager:
                 Intent paymentModeManagerIntent = new Intent(this, PaymentModeManagerActivity.class);
                 startActivity(paymentModeManagerIntent);
